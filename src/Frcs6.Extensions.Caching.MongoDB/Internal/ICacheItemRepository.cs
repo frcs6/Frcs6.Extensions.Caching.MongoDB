062c664538e6ac5070b7bbe64ed8010e720db139
Frcs6 <frcs6b@gmail.com>
2024-02-18 14:23:28 -0500

    Pass CancellationToken to all methods

--- src/Frcs6.Extensions.Caching.MongoDB/Internal/ICacheItemRepository.cs
+++ src/Frcs6.Extensions.Caching.MongoDB/Internal/ICacheItemRepository.cs
@@ -3,16 +3,17 @@ namespace Frcs6.Extensions.Caching.MongoDB.Internal;
 internal interface ICacheItemRepository
 {
     CacheItem Read(string key);
-    Task<CacheItem> ReadAsync(string key);
+    Task<CacheItem> ReadAsync(string key, CancellationToken token);
     CacheItem ReadPartial(string key);
-    Task<CacheItem> ReadPartialAsync(string key);
+    Task<CacheItem> ReadPartialAsync(string key, CancellationToken token);
     
     void Write(CacheItem cacheItem);
-    Task WriteAsync(CacheItem cacheItem);
+    Task WriteAsync(CacheItem cacheItem, CancellationToken token);
     void WritePartial(CacheItem cacheItem);
-    Task WritePartialAsync(CacheItem cacheItem);
+    Task WritePartialAsync(CacheItem cacheItem, CancellationToken token);
 
     void Remove(string key);
-    Task RemoveAsync(string key);
+    Task RemoveAsync(string key, CancellationToken token);
     void RemoveExpired();
+    Task RemoveExpiredAsync(CancellationToken token);
 }
