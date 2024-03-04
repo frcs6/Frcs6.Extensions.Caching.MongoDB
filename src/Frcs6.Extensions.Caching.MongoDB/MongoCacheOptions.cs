adcaf72bddeb2d1519795323654bffa854d2e66b
Frcs6 <frcs6b@gmail.com>
2024-03-04 13:01:11 -0500

    Update docs

--- src/Frcs6.Extensions.Caching.MongoDB/MongoCacheOptions.cs
+++ src/Frcs6.Extensions.Caching.MongoDB/MongoCacheOptions.cs
@@ -1,11 +1,29 @@
 namespace Frcs6.Extensions.Caching.MongoDB;
 
+/// <summary>
+/// Configuration options.
+/// </summary>
 [ExcludeFromCodeCoverage]
 public sealed class MongoCacheOptions : IOptions<MongoCacheOptions>
 {
+    /// <summary>
+    /// Cache database name.
+    /// </summary>
     public string? DatabaseName { get; set; }
+
+    /// <summary>
+    /// Cache collection name.
+    /// </summary>
     public string? CollectionName { get; set; }
+
+    /// <summary>
+    /// Allow cached item without.
+    /// </summary>
     public bool AllowNoExpiration { get; set; }
+
+    /// <summary>
+    /// Delay between remove task.
+    /// </summary>
     public TimeSpan? RemoveExpiredDelay { get; set; }
 
     MongoCacheOptions IOptions<MongoCacheOptions>.Value => this;
